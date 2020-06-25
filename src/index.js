b0bdafc3e60722b9ac6bea66608844bbd88833ee
fuston05 <fuston05@yahoo.com>
2020-06-25 12:55:32 -0400

    adds styling to start screen

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
 
+//styles
+import './index.scss';
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
