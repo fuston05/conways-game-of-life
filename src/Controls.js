d5c2a357a2f533e22744a02eb3b500fe7ab2c347
fuston05 <fuston05@yahoo.com>
2020-07-19 11:41:37 -0400

    moves controls to sidebar and consolidates all controls there.

--- src/Controls.js
+++ src/Controls.js
@@ -3,75 +3,126 @@ import './Controls.scss';
 
 //utils
 import { speedUpSim, slowDownSim } from './utils/speedControls';
-import { clearGrid } from './utils/gridFunctions';
+import { clearGrid, randomGrid } from './utils/gridFunctions';
 
 const Controls = ({
-  genCount,
-  setGenCount,
-  setIsRunning,
-  isRunningRef,
-  runSimulation,
-  speedDisplay,
-  setSpeedDisplay,
-  setSimSpeed,
-  simSpeed,
-  setGrid,
-  rowsCount,
-  colsCount
+    liveCellColor,
+    setLiveCellColor,
+    deadCellColor,
+    setDeadCellColor,
+    genCount,
+    setGenCount,
+    setIsRunning,
+    isRunningRef,
+    runSimulation,
+    speedDisplay,
+    setSpeedDisplay,
+    setSimSpeed,
+    simSpeed,
+    setGrid,
+    rowsCount,
+    colsCount
 }) => {
 
-  const startButton = document.querySelector('.start');
+    const ToggleDisableStart = () => {
+        const startButton = document.querySelector('.start');
+        startButton.setAttribute('disabled', 'true');
 
-  return (
-    <div className='controlsCont'>
+        setIsRunning(!isRunningRef.current);
+        isRunningRef.current = true;
+        runSimulation();
 
-      <button className='start'
-        onClick={() => {
-          startButton.setAttribute('disabled', 'true');
+        // re-enable start button
+        setTimeout(() => {
+            startButton.removeAttribute('disabled');
+        }, 1000)
+    }
 
-          setIsRunning(!isRunningRef.current);
-          isRunningRef.current = true;
-          runSimulation();
+    const handleliveChange = (e) => {
+        if (isRunningRef.current) {
+            return;
+        } else {
+            setLiveCellColor(e.target.value);
+        }
+    }
+    const handleDeadChange = (e) => {
+        if (isRunningRef.current === false) {
+            setDeadCellColor(e.target.value);
+        }
+    }
 
-          // re-enable start button
-          setTimeout(() => {
-            startButton && startButton.removeAttribute('disabled');
-          }, 1000)
-        }}
-      >
-        {isRunningRef.current ? 'Stop' : 'Start'}
-      </button>
+    return (
+        <div className='controlsCont'>
 
-      <button onClick={() => clearGrid(
-        setSimSpeed,
-        setGrid,
-        rowsCount,
-        colsCount,
-        setIsRunning,
-        isRunningRef,
-        setGenCount,
-        setSpeedDisplay
-      )}>Clear Grid/Reset</button>
+            <button className='start'
+                onClick={() => {
+                    ToggleDisableStart();
+                }}
+            >
+                {isRunningRef.current ? 'Stop' : 'Start'}
+            </button>
 
-      <span className= 'speedControlsConst'>
-        <button onClick={() => speedUpSim(
-          simSpeed,
-          setSimSpeed,
-          setSpeedDisplay
-        )}>faster &raquo;</button>
-  
-        <span className='speedDisplay'>&nbsp;<i> Speed: </i>{speedDisplay}&nbsp;</span>
-  
-        <button onClick={() => slowDownSim(
-          simSpeed,
-          setSimSpeed,
-          setSpeedDisplay
-        )}>Slower &laquo;</button>
-      </span>
+            <button onClick={() => randomGrid(
+                setIsRunning,
+                isRunningRef,
+                setGenCount,
+                setGrid,
+                rowsCount,
+                colsCount
+            )}>Randomize Grid</button>
 
-      <span className='generationDisplay'>Generation: {genCount}</span>
-    </div>
-  )
+            <button onClick={() => clearGrid(
+                setSimSpeed,
+                setGrid,
+                rowsCount,
+                colsCount,
+                setIsRunning,
+                isRunningRef,
+                setGenCount,
+                setSpeedDisplay
+            )}>Clear Grid/Reset</button>
+
+            <span className='speedControlsConst'>
+                <button onClick={() => speedUpSim(
+                    simSpeed,
+                    setSimSpeed,
+                    setSpeedDisplay
+                )}>faster &raquo;</button>
+
+                <span className='speedDisplay'>&nbsp;<i> Speed: </i>{speedDisplay}&nbsp;</span>
+
+                <button onClick={() => slowDownSim(
+                    simSpeed,
+                    setSimSpeed,
+                    setSpeedDisplay
+                )}>Slower &laquo;</button>
+            </span>
+
+            <div className='colorPickerCont'>
+                <div className='livePicker'>
+                    <label htmlFor='liveColorPicker'>Live Cell color: </label>
+                    <input
+                        onChange={(e) => { handleliveChange(e) }}
+                        name='liveColorPicker'
+                        type='color'
+                        value={liveCellColor}
+                    />
+                </div>
+
+                <div className='deadPicker'>
+                    <label htmlFor='deadColorPicker'>Dead Cell color: </label>
+                    <input
+                        onChange={(e) => { handleDeadChange(e) }}
+                        name='deadColorPicker'
+                        type='color'
+                        value={deadCellColor}
+                    />
+                </div>
+            </div>
+
+            <span className='generationDisplay'>Generation: {genCount}</span>
+        </div>
+    )
 }
 
 export default Controls;
--- src/Start_Screen.js
+++ src/Start_Screen.js
@@ -5,34 +5,35 @@ import { Link } from 'react-router-dom';
 import './Start_Screen.scss';
 
 const Start = () => {
-  return (
-    <div className='descCont'>
-      <div className='mask'></div>
-      <h1>Conway's Game of Life</h1>
-      <button><Link to='/game'>Check it out</Link></button>
-      <div className='textCont'>
-        <p>
-          Created by John Conway in 1970, in his pursuit to create an interesting and
-          unpredictable cellular automaton. Interestingly, he created this without the power of modern computing. Conway's Game of Life was created on a Go Board.
+    return (
+        <div className='descCont'>
+            <div className='mask'></div>
+            <h1>Conway's Game of Life</h1>
+            <button className= 'startButton'><Link to='/game'>Check it out</Link></button>
+
+            <div className='textCont'>
+                <p>
+                    Created by John Conway in 1970, in his pursuit to create an interesting and
+                    unpredictable cellular automaton. Interestingly, he created this without the power of modern computing. Conway's Game of Life was created on a Go Board.
         </p>
-        <p>
-          This game consists of a 2D grid and cells that exist in one of two states, 'alive' or 'dead'. This state is determined by the state of neighboring cells based  on 4 simple rules:
+                <p>
+                    This game consists of a 2D grid and cells that exist in one of two states, 'alive' or 'dead'. This state is determined by the state of neighboring cells based  on 4 simple rules:
         </p>
-        <ol>
-          <li>Any live cell with fewer than two live neighbors dies, as if by under population.</li>
-          <li>Any live cell with two or three live neighbors lives on to the next generation.</li>
-          <li>Any live cell with more than three live neighbors dies, as if by overpopulation.</li>
-          <li>Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.</li>
-        </ol>
-        <p>
-          You simply create the starting pattern, from there the game plays itself. You just sit back and watch as the cells evolve into very interesting shapes and move around the grid.
+                <ol>
+                    <li>Any live cell with fewer than two live neighbors dies, as if by under population.</li>
+                    <li>Any live cell with two or three live neighbors lives on to the next generation.</li>
+                    <li>Any live cell with more than three live neighbors dies, as if by overpopulation.</li>
+                    <li>Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.</li>
+                </ol>
+                <p>
+                    You simply create the starting pattern, from there the game plays itself. You just sit back and watch as the cells evolve into very interesting shapes and move around the grid.
         </p>
-        <p>
-          The Game of life is Turing Complete and can simulate any other Turing Machine, such as a universal constructor.
+                <p>
+                    The Game of life is Turing Complete and can simulate any other Turing Machine, such as a universal constructor.
         </p>
-      </div>
-    </div>
-  )
+            </div>
+        </div>
+    )
 }
 
 export default Start;
